430ba51acadbe7daedc00e98d228897692c30445
Jamie Webber <jamiecwebber@gmail.com>
2019-05-02 22:34:55 -0400

    successful file transfer

--- services/sounds/project/api/models.py
+++ services/sounds/project/api/models.py
@@ -8,13 +8,20 @@ from project import db
 
 class Sound(db.Model):
 
-    __tablename__ = 'sounds'
+	__tablename__ = 'sounds'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(128), nullable=False)
-    file_path = db.Column(db.String(128), nullable=False)
-    created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	title = db.Column(db.String(128), nullable=False)
+	file_path = db.Column(db.String(128), nullable=False)
+	created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
 
-    def __init__(self, title, file_path):
-        self.title = title
-        self.file_path = file_path
+	def to_json(self):
+		return {
+			'id': self.id,
+			'title': self.title,
+			'file_path': self.file_path
+		}
+
+	def __init__(self, title, file_path):
+		self.title = title
+		self.file_path = file_path
--- services/sounds/project/api/sounds.py
+++ services/sounds/project/api/sounds.py
@@ -1,7 +1,7 @@
 # services/sounds/project/api/sounds.py
 
 import os  # new
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, Blueprint, render_template
+from flask import Flask, flash, request, redirect, url_for, jsonify, send_from_directory, Blueprint, render_template
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy  # new
 from werkzeug.utils import secure_filename
@@ -27,28 +27,27 @@ def index():
 	return render_template('index.html')
 
 
+
 @sounds_blueprint.route('/sounds', methods=['GET', 'POST'])
-def upload_file():
-	flash("here")
+def sounds():
 	if request.method == 'POST':
 		# check if the post request has the file part
-		flash(request)
-		if 'file' not in request.files:
-			flash('No selected file')
-			return redirect(request.url)
 		file = request.files['file']
-		if file.filename == '':
-			flash("No selected file")
-			return redirect(request.url)
-		if file:
-			flash("yougotthisfar")
-			title = request.form['title']
-			filename = secure_filename(file.filename)
-			file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-			file.save(file_path)
-			db.session.add(Sound(title=title, file_path=file_path))
-			db.session.commit()
-			return redirect(url_for('uploaded_file', filename=filename))
+		title = request.form['title']
+		filename = secure_filename(file.filename)
+		file_path = os.path.join('./media', filename)
+		file.save(file_path)
+		db.session.add(Sound(title=title, file_path=file_path))
+		db.session.commit()
+		return 'success'
+	if request.method == 'GET':
+		response_object = {
+			'status': 'success',
+			'data': {
+				'sounds': [sound.to_json() for sound in Sound.query.all()]
+			}
+		}
+		return jsonify(response_object), 200
 
 
 @sounds_blueprint.route('/uploads/<filename>')
