f00d29fdd0ad550c0f00e40fb7d5b7b3ca700df4
Jamie Webber <jamiecwebber@gmail.com>
2019-05-02 21:12:05 -0400

    basic UI view up

--- services/sounds/project/api/sounds.py
+++ services/sounds/project/api/sounds.py
@@ -1,11 +1,15 @@
 # services/sounds/project/api/sounds.py
 
-
-from flask import Blueprint
+import os  # new
+from flask import Flask, flash, request, redirect, url_for, jsonify, send_from_directory, Blueprint, render_template
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
+from werkzeug.utils import secure_filename
+from project import db
+from project.api.models import Sound
 
 
-sounds_blueprint = Blueprint('sounds', __name__)
+sounds_blueprint = Blueprint('sounds', __name__, template_folder='./templates')
 api = Api(sounds_blueprint)
 
 
@@ -16,6 +20,35 @@ class SoundsPing(Resource):
         'message': 'pong!'
     }
 
-
-
 api.add_resource(SoundsPing, '/sounds/ping')
+
+@sounds_blueprint.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+
+@sounds_blueprint.route('/sounds', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No selected file')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash("No selected file")
+			return redirect(request.url)
+		if file:
+			title = request.form['title']
+			filename = secure_filename(file.filename)
+			file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+			file.save(file_path)
+			db.session.add(Sound(title=title, file_path=file_path))
+			db.session.commit()
+			return redirect(url_for('uploaded_file', filename=filename))
+
+
+@sounds_blueprint.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
