0c7fea09841904b6d61f34da6fa64904bd9778c5
Jamie Webber <jamiecwebber@gmail.com>
2019-05-10 13:05:07 -0400

    designing landing page

--- services/client/src/header.js
+++ services/client/src/header.js
@@ -7,7 +7,7 @@ class Header extends Component {
 	render () {
 		return (
 			<div id='header'>
-				<div className="headerSections"><img className='logo' src={logo} alt=''/> <h1>Red Vixen Music</h1><h2> - projects of Jamie Christopher Webber</h2></div>
+				<div className="headerSections"> <h1>Red Vixen Music</h1></div>
 				<div className="headerSections"><h2>web development</h2><h2>composition</h2><h2>audio programming</h2></div>
 			</div>
 		)
