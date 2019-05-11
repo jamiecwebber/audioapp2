1b94d736dfb7b0ee2b8e4a8c3a41c3722fc8097c
Jamie Webber <jamiecwebber@gmail.com>
2019-05-11 18:32:56 -0400

    done apart from composition page

--- services/client/src/soundsPage.js
+++ services/client/src/soundsPage.js
@@ -4,6 +4,8 @@ const SoundsPage = () => {
 	return (
 		<div>
 			<h2>Composition & Improvisation</h2>
+			<h3>coming soon</h3>
+			<h3>in the mean-time, check out all my music on my <a href='https://soundcloud.com/jamiechristopherwebber' target='blank'>Soundcloud page</a>.</h3>
 			
 		</div>
 	);
